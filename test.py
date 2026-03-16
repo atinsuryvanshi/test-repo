8fb2b6b5b60af7aae9909a2c844b30a755259ce4
atinsuryvanshi <139237526+atinsuryvanshi@users.noreply.github.com>
2026-03-16 20:40:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Flask Test Successful! PIP is working! 🚀</h1>"
+    # Maine emoji hata diya hai taaki Linux par koi encoding error na aaye
+    return "<h1>Flask Test Successful! PIP and Python are working perfectly!</h1>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # Yeh line AWS se directly correct port utha legi
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
